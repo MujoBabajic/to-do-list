64d62e6efc2598a1b13a7943295af5a5e89a7d75
Mujo <mbabajic.babajic56@gmail.com>
2024-02-02 03:28:53 +0100

    added Header component

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1>To-Do List</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
