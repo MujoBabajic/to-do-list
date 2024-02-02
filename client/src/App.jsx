1fde5062c9ca3cadc08ac64a5a087cc20c0019d2
Mujo <mbabajic.babajic56@gmail.com>
2024-02-02 04:29:42 +0100

    added AddTask component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 import "./App.css";
 
 import Header from "./components/Header/Header";
+import AddTask from "./components/AddTask/AddTask";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <div className="list-wrapper">
+        <AddTask></AddTask>
+      </div>
     </>
   );
 }
---
+++ client/src/components/AddTask/AddTask.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./AddTask.css";
+
+const AddTask = () => {
+  return (
+    <form>
+      <input></input>
+      <button>Add Task</button>
+    </form>
+  );
+};
+
+export default AddTask;
